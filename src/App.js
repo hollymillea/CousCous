5a48bbd85eae50ebf24a1ee1ecfbea9adc7a34c6
Holly Millea <hollymillea@gmail.com>
2024-10-07 18:32:45 +0100

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 // import Gallery from "./components/Gallery";
 // import About from "./components/About";
@@ -10,7 +10,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Home />
       {/* <Gallery /> */}
       {/* <About /> */}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,37 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa"; // Import a shopping cart icon from react-icons
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#gallery">Gallery</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#shop">Shop</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+      {/* Left Section: COUSCOUS */}
+      <div className="navbar-left">
+        <h1>COUSCOUS</h1>
+      </div>
+
+      {/* Center Section: Navigation Links */}
+      <div className="navbar-center">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#shop">Shop</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Right Section: Shopping Cart Icon */}
+      <div className="navbar-right">
+        <FaShoppingCart size={24} />
+      </div>
     </nav>
   );
 }
