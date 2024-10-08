ca1c38392437c1f0442bec77dbb4d4826e8afce7
Holly Millea <hollymillea@gmail.com>
2024-10-08 12:51:45 +0100

    Getting rid of add to basket

--- src/components/ItemPage.js
+++ src/components/ItemPage.js
@@ -1,60 +1,56 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import "./ItemPage.css";
+import holly from "./data";
 
 function ItemPage() {
   const { id } = useParams(); // Get artwork ID from URL
   const [selectedSize, setSelectedSize] = useState("A4");
 
   // Simulated data for artwork details
-  const artwork = {
-    id,
-    title: "DROPS 03",
-    price: { A4: "$155 USD", A3: "$185 USD" },
-    imgSrc: "/path-to-image-1.jpg",
-  };
-
-  const handleAddToBasket = () => {
-    alert(`Added ${artwork.title} (${selectedSize}) to basket!`);
-  };
+  const artwork = holly[id - 1];
 
   return (
-    <div className="artwork-detail">
-      <img src={artwork.imgSrc} alt={artwork.title} className="artwork-image" />
-      <h1>{artwork.title}</h1>
+    <div className="container">
+      <div className="container-left">
+        <img
+          src={artwork.imgSrc}
+          alt={artwork.title}
+          className="artwork-image"
+        />
+        <h1>{artwork.title}</h1>
 
-      <div className="artwork-price">
-        <h3>Price:</h3>
-        <p>{artwork.price[selectedSize]}</p>
+        <div className="artwork-price">
+          <h3>Price:</h3>
+          <p>{artwork.price[selectedSize]}</p>
+        </div>
       </div>
 
-      <div className="size-selector">
-        <h3>Select Size:</h3>
-        <label>
-          <input
-            type="radio"
-            name="size"
-            value="A4"
-            checked={selectedSize === "A4"}
-            onChange={() => setSelectedSize("A4")}
-          />
-          A4
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="size"
-            value="A3"
-            checked={selectedSize === "A3"}
-            onChange={() => setSelectedSize("A3")}
-          />
-          A3
-        </label>
+      <div className="container-right">
+        <div className="size-selector">
+          <h3>Select Size:</h3>
+          <label>
+            <input
+              type="radio"
+              name="size"
+              value="A4"
+              checked={selectedSize === "A4"}
+              onChange={() => setSelectedSize("A4")}
+            />
+            A4
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="size"
+              value="A3"
+              checked={selectedSize === "A3"}
+              onChange={() => setSelectedSize("A3")}
+            />
+            A3
+          </label>
+        </div>
       </div>
-
-      <button className="add-to-basket" onClick={handleAddToBasket}>
-        Add to Basket
-      </button>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
       <div className="navbar-center">
         <ul>
           <li>
-            <a href="#home">Home</a>
+            <a href="/">Home</a>
           </li>
           <li>
             <a href="#about">About</a>
