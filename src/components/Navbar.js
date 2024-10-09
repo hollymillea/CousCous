ad2b34df74d972f30313115282c96273bfe0b700
Holly Millea <hollymillea@gmail.com>
2024-10-09 21:22:42 +0100

    Added menu popup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { FiMenu } from "react-icons/fi"; // Import a shopping cart icon from react-icons
+import React, { useState } from "react";
+import { FiMenu } from "react-icons/fi"; // Import a menu icon
 import "./Navbar.css";
 
 function Navbar() {
+  // State to manage menu visibility
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Toggle the menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       {/* Left Section: SVG Cous Cous */}
@@ -11,10 +19,22 @@ function Navbar() {
         <h1 className="couscous2 graffiti">Cous</h1>
       </div>
 
-      {/* Right Section: Shopping Cart Icon */}
+      {/* Right Section: Menu Icon */}
       <div className="navbar-right">
-        <FiMenu size={24} />
+        <FiMenu size={24} onClick={toggleMenu} />
       </div>
+
+      {/* Pop-Out Menu */}
+      {isMenuOpen && (
+        <div className="pop-out-menu">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
